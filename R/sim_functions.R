69cfacd239c9a8632e2847986a06fa9cb784a1f8
Jonathan Levy <jl@jlstiles.com>
2017-07-01 13:25:01 -0700

    fcns

--- R/sim_functions.R
+++ R/sim_functions.R
@@ -1,5 +1,6 @@
 
 # function to check MSE
+#' @export
 perf=function(ests,truth){
   n=length(ests)
   var=((n-1)/n)*var(ests)
@@ -8,6 +9,7 @@ perf=function(ests,truth){
   c(var=var,bias=bias,mse=mse)
 }
 
+#' @export
 gendata=function(n,g0, Q0){
   W1 = runif(n,-3,3)
   W2=rnorm(n)
@@ -18,35 +20,39 @@ gendata=function(n,g0, Q0){
   data.frame(A,W1,W2,W3,W4,Y)
 }
 
-
+#' @export
 Q0_trig1= function (A, W1, W2, W3, W4) 
 {
   plogis(.14*(2* A + 2*A * W1 + 20*cos(W1) * A - 3*W1 * sin(2*W2)+ cos(W1)
               -3*W2+4*A*(W2^2) +3*cos(W4)*A +A*W1^2- 2 * sin(W2)*W4 - 6*A* W3 * W4-3))
 }
 
+#' @export
 Q0_trig =function (A, W1, W2, W3, W4)
 {
   plogis(.14*(2* A  + 20*cos(W1) * A +cos(W1)-4*A*(W2^2) +3*cos(W4)*A +A*W1^2))
 }
 
+#' @export
 Q0_1 = function (A, W1, W2, W3, W4) 
 {
   plogis(.14*(2* A + 2*A * W1 + 4*A*W3*W4+W2*W1+W3*W4+10*A*cos(W4)))
 }
 
 
-
+#' @export
 g0_linear= function (W1, W2, W3, W4) 
 {
   plogis(.5*(-0.8 * W1 + 0.39 * W2 + 0.08 * W3 - 0.12 * W4 - 0.15))
 }
 
+#' @export
 g0_1 = function (W1, W2, W3, W4) 
 {
   plogis(.5*(-0.08 * W1^2*W2+.5*W1 + 0.49 * cos(W2)*W3 + 0.18 * W3^2 - 0.12 * sin(W4) - 0.15))
 }
 
+#' @export
 sim_lr = function(n, g0, Q0, form) {
   
   simdata = gendata(n, g0, Q0)
@@ -68,6 +74,7 @@ sim_lr = function(n, g0, Q0, form) {
   return(initest)
 }
 
+#' @export
 sim_hal = function(n, g0, Q0, HAL, SL.library, SL.libraryG) {
   
   simdata = gendata(n, g0, Q0)
