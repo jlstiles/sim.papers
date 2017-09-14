791adb0fa51ee7bb1f88eadf5d1d13d1ff10b4aa
Jonathan Levy <jl@jlstiles.com>
2017-09-14 02:03:15 -0700

    big sims of 100

--- case3.2.R
+++ case3.2.R
@@ -6,21 +6,37 @@ source(source_file)
 library(Simulations)
 source("WrappersVblip1.R")
 
+SL.library1
+SL.libraryG
+
 SL.library = SL.library1
 SL.libraryG = SL.libraryG
 
+detectCores()
 cl = makeCluster(detectCores(), type = "SOCK")
 registerDoSNOW(cl)
 clusterExport(cl,cl_export)
 n=1000
 B=100
+
 g0 = g0_1
 Q0 = Q0_2
+# debug(SL.stack1)
+# debug(sim_cv)
 ALL=foreach(i=1:B,.packages=c("gentmle2","mvtnorm","hal","Simulations","SuperLearner"),
             .errorhandling = "remove")%dopar%
-            {sim_cv(n, g0 = g0, Q0 = Q0, SL.library=SL.library, 
-                    SL.libraryG=SL.libraryG,method = "method.NNloglik",cv=TRUE
+            {sim_cv(n, g0 = g0, Q0 = Q0, SL.library = SL.library,
+                    SL.libraryG = SL.libraryG, method = "method.NNLS", cv = TRUE, V = 10, SL = 10L, single = TRUE
             )}
 results = data.matrix(data.frame(do.call(rbind, ALL)))
 
-save(results, ALL, file = "case3.2.RData")
+# ALL = list()
+# for (it in 1:6) {
+#   ALL[[it]] = foreach(i=1:B,.packages=c("gentmle2","mvtnorm","hal","Simulations","SuperLearner"),
+#                      .errorhandling = "remove")%dopar%
+#                      {sim_cv(n, g0 = g0, Q0 = Q0, SL.library = SL.library, 
+#                              SL.libraryG = SL.libraryG[c(1:3,5:7)[1:it]], method = "method.NNLS", cv = TRUE, V = 2, SL = 2L, single = TRUE
+#                      )}}
+# 
+# lapply(ALL, length)
+save(ALL, file = "case3.2.RData")
