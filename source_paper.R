e0fc938f4d8cc0af103d5357e6ad338199542025
Jonathan Levy <jl@jlstiles.com>
2017-09-22 21:15:27 -0700

    there was no screenFull uhhh

--- source_paper.R
+++ source_paper.R
@@ -27,7 +27,7 @@ if (case == "setup") {
                      c("rpartPruneSL", "screen.Main"),"xgbFull",c("xgbMain","screen.Main"),
                      c("xgb6","screen6"), c("xgb10","screen10"),
                      c("nnetMain","screen.Main"), c("earthMain","screen.Main"),
-                     c("earthFull","screenFull"),
+                     "earthFull",
                      c("ranger10", "screen10", "All"),"nnetFull",
                      c("SL.glm","screen.Main","screen6","screen10","All"),
                      "SL.stepAIC", c("SL.hal","screen.Main"),"SL.mean","glm.mainint")
