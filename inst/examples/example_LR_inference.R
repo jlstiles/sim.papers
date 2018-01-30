fc62e3c45c2ab7bb3e9294db71e90fbf0521a98e
Jonathan Levy <jl@jlstiles.com>
2018-01-29 23:28:36 -0800

    switching ATE to be first then blipVariance in the output.  mean then variance

--- inst/examples/example_LR_inference.R
+++ inst/examples/example_LR_inference.R
@@ -1,8 +1,17 @@
-# using built-in package functions, g0_linear and Q0_1 to specify
+# using built-in package functions, g0_linear and define Q0_linear to specify
 # pscore and outcome model probabilities
+
+g0_linear
+Q0_linear = function(A,W1,W2,W3,W4) plogis(A + W1 + W2 + A*(W3 + W4))
+
+# get a randomly drawn dataframe under the specified model
 data = gendata(1000, g0_linear, Q0_1)
 
-Qform = formula("Y ~ W2 + W3 + A*W4")
+# get the truth
+truth = get.truth(g0_linear, Q0_linear)
+truth
+
+Qform = formula("Y ~ W1 + W2 + A*(W3 + W4)")
 
 # specifying the covariates, treatment and outcome
 W = data[,2:5]
