e2b2fcd56cd92305d010680e7090ca713a3c58e6
Jonathan Levy <jl@jlstiles.com>
2018-02-13 01:17:41 -0800

    setA  is much to determine the length for now

--- R/LR.inference.R
+++ R/LR.inference.R
@@ -63,7 +63,7 @@ IC.beta = function(data,OC=NULL, Ynode, Anodes, Qform) {
 #' @example /inst/examples/example_longTSM.R
 long.TSM = function(data, Ynodes, Anodes, formulas, setA, alpha = .05)
 {
-  times = 1:length(formulas)
+  times = 1:length(setA)
   times = times[order(times,decreasing = TRUE)]
   for (t in times){
     # undebug(IC.beta)
